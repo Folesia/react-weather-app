7a8d91836c9f7df5edfac0733cd2556bbdc6db0a
Folesia <folesia@hotmail.com>
2023-05-09 12:01:52 -0500

    Added basic stilling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-
-import './App.css';
-
-
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-  <footer>
-    This project was coded by Olesia Fatenko and is{" "}
-  <a href="https://github.com/Folesia/react-weather-app" target="_blank">
- open-sourced on GitHub
-  </a>
-  </footer>
-  </div>;
+  return (
+    <div className="App">
+      <div className="container">
+            <Weather />
+      <footer>
+        This project was coded by Olesia Fatenko and is{" "}
+        <a href="https://github.com/Folesia/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+      </div>
+    </div>
+  );
 }
