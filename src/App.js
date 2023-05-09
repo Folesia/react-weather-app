0b9c2b944b6c3b45f7bd6a5e7c985f1dff07bb19
Folesia <folesia@hotmail.com>
2023-05-08 23:53:25 -0500

    added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -5,5 +5,13 @@ import './App.css';
 
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+  <footer>
+    This project was coded by Olesia Fatenko and is{" "}
+  <a href="https://github.com/Folesia/react-weather-app" target="_blank">
+ open-sourced on GitHub
+  </a>
+  </footer>
+  </div>;
 }
